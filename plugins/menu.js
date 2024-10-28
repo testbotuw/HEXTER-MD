9a906bf63d586cc2553992753f9e6ca5c078f3fe
testbotuw <tharulweb@gmail.com>
2024-10-28 13:01:36 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,28 +1,19 @@
-require("../settings")
-const { getMenu, getAlive } = require("hexter-md");
-const commands = [
-  {
-  name: "menu",
-  alias: ["h", "panel"],
-  desc: "List all command",
-  category: "General",
-  react: "✅",
-  start: async (client, m, { commands, args, prefix, text, toUpper }) => {
-    await getMenu(client, m, { commands, args, prefix, text, toUpper })
-  }
-},
+/*
 
-//============================================================
+██   ██ ███████ ██   ██ ████████ ███████ ██████      ███    ███ ██████      ██     ██ ██   ██  █████  ████████ ███████  █████  ██████  ██████      ██████   ██████  ████████ 
+██   ██ ██       ██ ██     ██    ██      ██   ██     ████  ████ ██   ██     ██     ██ ██   ██ ██   ██    ██    ██      ██   ██ ██   ██ ██   ██     ██   ██ ██    ██    ██    
+███████ █████     ███      ██    █████   ██████      ██ ████ ██ ██   ██     ██  █  ██ ███████ ███████    ██    ███████ ███████ ██████  ██████      ██████  ██    ██    ██    
+██   ██ ██       ██ ██     ██    ██      ██   ██     ██  ██  ██ ██   ██     ██ ███ ██ ██   ██ ██   ██    ██         ██ ██   ██ ██      ██          ██   ██ ██    ██    ██    
+██   ██ ███████ ██   ██    ██    ███████ ██   ██     ██      ██ ██████       ███ ███  ██   ██ ██   ██    ██    ███████ ██   ██ ██      ██          ██████   ██████     ██    
+                                                                                                                                                                             
+                                                                                                                                                                             
+Project Name : Your Project Name
+Developed By ; Your Name
+Version : 1.0
 
-{
-  name: "alive",
-  react: "🍁",
-  category: "General",
-  start: async(client, m, { pushName }) => {
-    await getAlive(client, m, { pushName })
-  }
-}
+Enter Your Details As You Wish
 
-]
+Dont,t Try To Decrypt My Code.
+*/
 
-module.exports = commands;
+function _0xaf08(){const _0x4d76be=['alive','13223740XYLkKM','3173608zSdfDg','326XtjZkN','4953416mOIyJH','9tanALM','List\x20all\x20command','2623710msfCEb','604pWuqXI','1311085ohcErR','hexter-md','panel','General','7MZFTjE','3216516ekeYVk'];_0xaf08=function(){return _0x4d76be;};return _0xaf08();}const _0x10a1e6=_0x1383;(function(_0x3baf9d,_0x529ada){const _0x2e4565=_0x1383,_0x23a04e=_0x3baf9d();while(!![]){try{const _0x18ab8b=parseInt(_0x2e4565(0x7c))/0x1*(parseInt(_0x2e4565(0x81))/0x2)+parseInt(_0x2e4565(0x78))/0x3+parseInt(_0x2e4565(0x7b))/0x4+-parseInt(_0x2e4565(0x82))/0x5+-parseInt(_0x2e4565(0x80))/0x6*(parseInt(_0x2e4565(0x77))/0x7)+parseInt(_0x2e4565(0x7d))/0x8+parseInt(_0x2e4565(0x7e))/0x9*(-parseInt(_0x2e4565(0x7a))/0xa);if(_0x18ab8b===_0x529ada)break;else _0x23a04e['push'](_0x23a04e['shift']());}catch(_0x3f6ce3){_0x23a04e['push'](_0x23a04e['shift']());}}}(_0xaf08,0x890af),require('../settings'));function _0x1383(_0xe90b2b,_0xd4bc9b){const _0xaf0822=_0xaf08();return _0x1383=function(_0x1383c6,_0x5b854e){_0x1383c6=_0x1383c6-0x74;let _0x1cc891=_0xaf0822[_0x1383c6];return _0x1cc891;},_0x1383(_0xe90b2b,_0xd4bc9b);}const {getMenu,getAlive}=require(_0x10a1e6(0x74)),commands=[{'name':'menu','alias':['h',_0x10a1e6(0x75)],'desc':_0x10a1e6(0x7f),'category':_0x10a1e6(0x76),'react':'✅','start':async(_0x1d4080,_0x330d12,{commands:_0x58fcd5,args:_0x1432c6,prefix:_0x471511,text:_0x307c75,toUpper:_0x5aefd3})=>{await getMenu(_0x1d4080,_0x330d12,{'commands':_0x58fcd5,'args':_0x1432c6,'prefix':_0x471511,'text':_0x307c75,'toUpper':_0x5aefd3});}},{'name':_0x10a1e6(0x79),'react':'🍁','category':_0x10a1e6(0x76),'start':async(_0x5e7765,_0x3851da,{pushName:_0x3a26a6})=>{await getAlive(_0x5e7765,_0x3851da,{'pushName':_0x3a26a6});}}];module['exports']=commands;
