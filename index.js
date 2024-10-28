ffb9ef7db0112203395a4ad832e8313539eff725
testbotuw <tharulweb@gmail.com>
2024-10-28 12:58:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
-const axios = require("axios")
+/*
 
-const getCommand = async (client, m,command, prefix, text, args) => {
-    try {
-        const response = await axios.get(`https://far-cuckoo-nimeshofficial15-853fcda0.koyeb.app/derana-news`);
-            
-        const caption = `Title: ${response.data.result.title}
-        
-        Date: ${response.data.result.date}
-        
-        Description: ${response.data.result.description}
-        
-        *ʜᴇxᴛᴇʀ-ᴍᴅ ʙᴇᴛᴀ ᴠᴇʀsɪᴏɴ*
-        *ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴇᴠᴇʟᴏᴄᴏʀᴇ ᴘᴠᴛ ʟᴛᴅ*`;
-        
-        await client.sendMessage(m.from, { image: { url: response.data.result.image }, caption: caption }, { quoted: m });
-    } catch (error) {
-      console.error('Error fetching commands:', error);
-      throw error; // Optionally rethrow or handle it differently
-    }
-  };
-  
-  module.exports = { getCommand };
+██   ██ ███████ ██   ██ ████████ ███████ ██████      ███    ███ ██████      ██     ██ ██   ██  █████  ████████ ███████  █████  ██████  ██████      ██████   ██████  ████████ 
+██   ██ ██       ██ ██     ██    ██      ██   ██     ████  ████ ██   ██     ██     ██ ██   ██ ██   ██    ██    ██      ██   ██ ██   ██ ██   ██     ██   ██ ██    ██    ██    
+███████ █████     ███      ██    █████   ██████      ██ ████ ██ ██   ██     ██  █  ██ ███████ ███████    ██    ███████ ███████ ██████  ██████      ██████  ██    ██    ██    
+██   ██ ██       ██ ██     ██    ██      ██   ██     ██  ██  ██ ██   ██     ██ ███ ██ ██   ██ ██   ██    ██         ██ ██   ██ ██      ██          ██   ██ ██    ██    ██    
+██   ██ ███████ ██   ██    ██    ███████ ██   ██     ██      ██ ██████       ███ ███  ██   ██ ██   ██    ██    ███████ ██   ██ ██      ██          ██████   ██████     ██    
+                                                                                                                                                                             
+                                                                                                                                                                             
+Project Name : Your Project Name
+Developed By ; Your Name
+Version : 1.0
+
+Enter Your Details As You Wish
+
+Dont,t Try To Decrypt My Code.
+*/
+
+
+const _0x5d233=_0x2b1a;function _0x2b1a(_0x55b5f4,_0x3955bd){const _0xb59abf=_0xb59a();return _0x2b1a=function(_0x2b1aa2,_0x332f63){_0x2b1aa2=_0x2b1aa2-0x150;let _0xad6cb0=_0xb59abf[_0x2b1aa2];return _0xad6cb0;},_0x2b1a(_0x55b5f4,_0x3955bd);}function _0xb59a(){const _0x3fa61d=['data','1768070OgPDrc','759716JkQCOk','from','sendMessage','5Rakqej','1897936flKPDv','168FvasOy','Error\x20fetching\x20commands:','result','title','Title:\x20','image','\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x0a\x20\x20\x20\x20\x20\x20\x20\x20Date:\x20','59521bHFoKb','axios','47ZGYNXT','522378IuhbmD','\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x0a\x20\x20\x20\x20\x20\x20\x20\x20*ʜᴇxᴛᴇʀ-ᴍᴅ\x20ʙᴇᴛᴀ\x20ᴠᴇʀsɪᴏɴ*\x0a\x20\x20\x20\x20\x20\x20\x20\x20*ᴘᴏᴡᴇʀᴇᴅ\x20ʙʏ\x20ᴇᴠᴇʟᴏᴄᴏʀᴇ\x20ᴘᴠᴛ\x20ʟᴛᴅ*','1400436MRUrBk','https://far-cuckoo-nimeshofficial15-853fcda0.koyeb.app/derana-news','error','description','7530ACVoIF'];_0xb59a=function(){return _0x3fa61d;};return _0xb59a();}(function(_0xb3a338,_0x5ca0cb){const _0x19806b=_0x2b1a,_0x1bf17d=_0xb3a338();while(!![]){try{const _0x38425a=parseInt(_0x19806b(0x154))/0x1*(-parseInt(_0x19806b(0x15b))/0x2)+parseInt(_0x19806b(0x155))/0x3+-parseInt(_0x19806b(0x15e))/0x4*(-parseInt(_0x19806b(0x161))/0x5)+-parseInt(_0x19806b(0x163))/0x6*(parseInt(_0x19806b(0x152))/0x7)+parseInt(_0x19806b(0x162))/0x8+parseInt(_0x19806b(0x157))/0x9+-parseInt(_0x19806b(0x15d))/0xa;if(_0x38425a===_0x5ca0cb)break;else _0x1bf17d['push'](_0x1bf17d['shift']());}catch(_0x36b8ab){_0x1bf17d['push'](_0x1bf17d['shift']());}}}(_0xb59a,0x284bf));const axios=require(_0x5d233(0x153)),getCommand=async(_0x1e15ce,_0x5c011a,_0x49a5a1,_0x2157d9,_0x182357,_0x443769)=>{const _0x5f503e=_0x5d233;try{const _0x1cd5c9=await axios['get'](_0x5f503e(0x158)),_0x3c28d3=_0x5f503e(0x167)+_0x1cd5c9[_0x5f503e(0x15c)]['result'][_0x5f503e(0x166)]+_0x5f503e(0x151)+_0x1cd5c9[_0x5f503e(0x15c)][_0x5f503e(0x165)]['date']+'\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x0a\x20\x20\x20\x20\x20\x20\x20\x20Description:\x20'+_0x1cd5c9[_0x5f503e(0x15c)][_0x5f503e(0x165)][_0x5f503e(0x15a)]+_0x5f503e(0x156);await _0x1e15ce[_0x5f503e(0x160)](_0x5c011a[_0x5f503e(0x15f)],{'image':{'url':_0x1cd5c9['data']['result'][_0x5f503e(0x150)]},'caption':_0x3c28d3},{'quoted':_0x5c011a});}catch(_0x3c0ac9){console[_0x5f503e(0x159)](_0x5f503e(0x164),_0x3c0ac9);throw _0x3c0ac9;}};module['exports']={'getCommand':getCommand};
