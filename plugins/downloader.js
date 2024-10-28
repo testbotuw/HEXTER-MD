e89a0404de727942abbfb18822ee8b00b6e7d205
testbotuw <tharulweb@gmail.com>
2024-10-28 13:00:56 +0530

    Update downloader.js

--- plugins/downloader.js
+++ plugins/downloader.js
@@ -1,35 +1,19 @@
-require("../settings");
-const { getSong, getYtmp3, getYtDoc } = require("hexter-md");
+/*
 
-const commands = [
-  {
-    name: "song",
-    desc: "Plays the song...",
-    category: "Media",
-    react: "🎧",
-    start: async (client, m, { command, prefix, text, args }) => {
-      await getSong(client, m, { command, prefix, text, args });
-    },
-  },
+██   ██ ███████ ██   ██ ████████ ███████ ██████      ███    ███ ██████      ██     ██ ██   ██  █████  ████████ ███████  █████  ██████  ██████      ██████   ██████  ████████ 
+██   ██ ██       ██ ██     ██    ██      ██   ██     ████  ████ ██   ██     ██     ██ ██   ██ ██   ██    ██    ██      ██   ██ ██   ██ ██   ██     ██   ██ ██    ██    ██    
+███████ █████     ███      ██    █████   ██████      ██ ████ ██ ██   ██     ██  █  ██ ███████ ███████    ██    ███████ ███████ ██████  ██████      ██████  ██    ██    ██    
+██   ██ ██       ██ ██     ██    ██      ██   ██     ██  ██  ██ ██   ██     ██ ███ ██ ██   ██ ██   ██    ██         ██ ██   ██ ██      ██          ██   ██ ██    ██    ██    
+██   ██ ███████ ██   ██    ██    ███████ ██   ██     ██      ██ ██████       ███ ███  ██   ██ ██   ██    ██    ███████ ██   ██ ██      ██          ██████   ██████     ██    
+                                                                                                                                                                             
+                                                                                                                                                                             
+Project Name : Your Project Name
+Developed By ; Your Name
+Version : 1.0
 
-  //============================================================
+Enter Your Details As You Wish
 
-  {
-    name: "ytdoc",
-    category: "Media",
-    react: "🎧",
-    start: async (client, m, { command, prefix, text, args }) => {
-        await getYtDoc(client, m, { command, prefix, text, args })
-  }},
+Dont,t Try To Decrypt My Code.
+*/
 
-  //============================================================
-
-  {
-    name: "ytmp3",
-    category: "Media",
-    react: "🎧",
-    start: async (client, m, { command, prefix, text, args }) => {
-      await getYtmp3(client, m, { command, prefix, text, args })
-  }},
-];
-module.exports = commands;
+function _0x3f95(_0x13fa31,_0x4d8a5f){const _0x5d4213=_0x5d42();return _0x3f95=function(_0x3f9546,_0x593298){_0x3f9546=_0x3f9546-0x96;let _0x5daee5=_0x5d4213[_0x3f9546];return _0x5daee5;},_0x3f95(_0x13fa31,_0x4d8a5f);}const _0x4a6655=_0x3f95;function _0x5d42(){const _0x41f815=['hexter-md','36HhmOQy','song','ytmp3','Media','789448mNyOUb','../settings','207818uzBGRt','622152NllGCL','25jVPGYO','1xgqbxs','7GBOdqn','27eUtNof','1026558OCoHCf','753110NtCLjb','5388MIaPXH','842193NCKbMd','exports'];_0x5d42=function(){return _0x41f815;};return _0x5d42();}(function(_0x3e411f,_0x99a1bd){const _0x2079fc=_0x3f95,_0x895855=_0x3e411f();while(!![]){try{const _0x5ed0c=-parseInt(_0x2079fc(0x9e))/0x1*(-parseInt(_0x2079fc(0x9b))/0x2)+parseInt(_0x2079fc(0x9c))/0x3+parseInt(_0x2079fc(0xa3))/0x4*(parseInt(_0x2079fc(0x9d))/0x5)+-parseInt(_0x2079fc(0xa1))/0x6*(parseInt(_0x2079fc(0x9f))/0x7)+-parseInt(_0x2079fc(0x99))/0x8*(parseInt(_0x2079fc(0xa0))/0x9)+parseInt(_0x2079fc(0xa2))/0xa+-parseInt(_0x2079fc(0xa4))/0xb*(-parseInt(_0x2079fc(0xa7))/0xc);if(_0x5ed0c===_0x99a1bd)break;else _0x895855['push'](_0x895855['shift']());}catch(_0x56ce49){_0x895855['push'](_0x895855['shift']());}}}(_0x5d42,0x260f4),require(_0x4a6655(0x9a)));const {getSong,getYtmp3,getYtDoc}=require(_0x4a6655(0xa6)),commands=[{'name':_0x4a6655(0x96),'desc':'Plays\x20the\x20song...','category':_0x4a6655(0x98),'react':'🎧','start':async(_0x16c79f,_0x18ce64,{command:_0xada256,prefix:_0x4b5254,text:_0x314c3a,args:_0x2f7029})=>{await getSong(_0x16c79f,_0x18ce64,{'command':_0xada256,'prefix':_0x4b5254,'text':_0x314c3a,'args':_0x2f7029});}},{'name':'ytdoc','category':_0x4a6655(0x98),'react':'🎧','start':async(_0x2de015,_0x5cb49f,{command:_0x410bbe,prefix:_0x4e0943,text:_0x2e1370,args:_0x3eb390})=>{await getYtDoc(_0x2de015,_0x5cb49f,{'command':_0x410bbe,'prefix':_0x4e0943,'text':_0x2e1370,'args':_0x3eb390});}},{'name':_0x4a6655(0x97),'category':_0x4a6655(0x98),'react':'🎧','start':async(_0x2b32dd,_0x149e9d,{command:_0x301b37,prefix:_0x1b576f,text:_0x27c9af,args:_0x3dd7f2})=>{await getYtmp3(_0x2b32dd,_0x149e9d,{'command':_0x301b37,'prefix':_0x1b576f,'text':_0x27c9af,'args':_0x3dd7f2});}}];module[_0x4a6655(0xa5)]=commands;
